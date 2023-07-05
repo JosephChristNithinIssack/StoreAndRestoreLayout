67be8e6ed942a9d86a430a02fe2e319fa6afe319
Joseph Christ Nithin I <69190287+JosephChristNithinIssack@users.noreply.github.com>
2023-07-05 13:39:43 +0530

    Add files via upload

---
+++ netCore6x/Controllers/HomeController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netCore6x.Models;
+using System.Diagnostics;
+using Syncfusion.EJ2.Base;
+using System.Data;
+using System.Text;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using Microsoft.VisualBasic;
+
+namespace netCore6x.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public int indexcount = 0;
+        public static string persistedData;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UrlDatasource([FromBody] DataManagerRequest dm)
+        {
+            IEnumerable DataSource =  new List<OrdersDetails>();
+            int count = OrdersDetails.GetAllRecords().Cast<object>().Count();
+            //if (dm.Where != null && dm.Where.Count > 0) //Filtering
+            //{
+                DataSource = OrdersDetails.GetAllRecords();
+                DataOperations operation = new DataOperations();
+                if (dm.Search != null && dm.Search.Count > 0)
+                {
+                    DataSource = operation.PerformSearching(DataSource, dm.Search);  //Search
+                }
+                if (dm.Sorted != null && dm.Sorted.Count > 0) //Sorting
+                {
+                    DataSource = operation.PerformSorting(DataSource, dm.Sorted);
+                }
+                if (dm.Where != null && dm.Where.Count > 0) //Filtering
+                {
+                    DataSource = operation.PerformFiltering(DataSource, dm.Where, dm.Where[0].Operator);
+                }
+                if (dm.Skip != 0)
+                {
+                    DataSource = operation.PerformSkip(DataSource, dm.Skip);   //Paging
+                }
+                if (dm.Take != 0)
+                {
+                    DataSource = operation.PerformTake(DataSource, dm.Take);
+                }
+                return dm.RequiresCounts ? Json(new { result = DataSource, count = count }) : Json(DataSource);
+            //} else
+            //{
+            //    return Json(new { result = DataSource, count = DataSource.Cast<object>().Count() });
+            //}
+        }
+
+
+        public IActionResult StorePersistData([FromBody] StoreData persistData)
+        {
+            persistedData = persistData.persistData;
+            return new JsonResult(persistedData);
+        }
+
+        public IActionResult Restore()
+        {
+            return Content(persistedData);
+        }
+
+        public class StoreData
+        {
+            public string? persistData { get; set; }
+        }
+
+    }
+
+    public class OrdersDetails
+    {
+        public static List<OrdersDetails> order = new List<OrdersDetails>();
+        public OrdersDetails()
+        {
+
+        }
+        public OrdersDetails(int OrderID, string CustomerId, int EmployeeId, ComplexOrdersDetails Freight, bool Verified, DateTime OrderDate, string ShipCity, string ShipName, string ShipCountry, DateTime ShippedDate, string ShipAddress)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            this.ShipCity = ShipCity;
+            this.Verified = Verified;
+            this.OrderDate = OrderDate;
+            this.ShipName = ShipName;
+            this.ShipCountry = ShipCountry;
+            this.ShippedDate = ShippedDate;
+            this.ShipAddress = ShipAddress;
+        }
+        public static List<OrdersDetails> GetAllRecords()
+        {
+            if (order.Count() == 0)
+            {
+                int code = 10000;
+                for (int i = 1; i < 5; i++)
+                {
+                    order.Add(new OrdersDetails(code + 1, "ALFKI", i + 0, new ComplexOrdersDetails(0), false, new DateTime(1991, 05, 15), "Berlin", "Simons bistro", "Denmark", new DateTime(1996, 7, 16), "Kirchgasse 6"));
+                    order.Add(new OrdersDetails(code + 2, "ANATR", i + 2, new ComplexOrdersDetails(3.3 * i), true, new DateTime(1990, 04, 04), "Madrid", "Queen Cozinha", "Brazil", new DateTime(1996, 9, 11), "Avda. Azteca 123"));
+                    order.Add(new OrdersDetails(code + 3, "ANTON", i + 1, new ComplexOrdersDetails(0), true, new DateTime(1957, 11, 30), "Cholchester", "Frankenversand", "Germany", new DateTime(1996, 10, 7), "Carrera 52 con Ave. BolĂ­var #65-98 Llano Largo"));
+                    order.Add(new OrdersDetails(code + 4, "BLONP", i + 3, new ComplexOrdersDetails(5.3 * i), false, new DateTime(1930, 10, 22), "Marseille", "Ernst Handel", "Austria", new DateTime(1996, 12, 30), "Magazinweg 7"));
+                    order.Add(new OrdersDetails(code + 5, "BOLID", i + 4, new ComplexOrdersDetails(6.3 * i), true, new DateTime(1953, 02, 18), "Tsawassen", "Hanari Carnes", "Switzerland", new DateTime(1997, 12, 3), "1029 - 12th Ave. S."));
+                    code += 5;
+                }
+            }
+            return order;
+        }
+
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public ComplexOrdersDetails? Freight { get; set; }
+        public string ShipCity { get; set; }
+        public bool Verified { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public DateTime ShippedDate { get; set; }
+        public string ShipAddress { get; set; }
+    }
+
+    public class ComplexOrdersDetails
+    {  
+        public ComplexOrdersDetails()
+        {
+
+        }
+        public ComplexOrdersDetails(double Freight)
+        {
+            this.Freight = Freight;
+        }
+        
+
+        public double? Freight { get; set; }
+    }
+}
