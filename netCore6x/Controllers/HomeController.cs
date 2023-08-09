d00a8e0d12ae8bfecf09b2d7a35477cafea6d878
Joseph Christ Nithin Issack <josephchristnithin.issack@syncfusion.com>
2023-08-09 12:27:01 +0530

    Changes done

--- netCore6x/Controllers/HomeController.cs
+++ netCore6x/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using netCore6x.Models;
 using System.Diagnostics;
 using Syncfusion.EJ2.Base;
@@ -23,48 +23,19 @@ namespace netCore6x.Controllers
 
         public IActionResult Index()
         {
+            var Order = OrdersDetails.GetAllRecords();
+            ViewBag.DataSource = Order;
             return View();
         }
-
-        public IActionResult UrlDatasource([FromBody] DataManagerRequest dm)
-        {
-            IEnumerable DataSource =  new List<OrdersDetails>();
-            int count = OrdersDetails.GetAllRecords().Cast<object>().Count();
-                DataSource = OrdersDetails.GetAllRecords();
-                DataOperations operation = new DataOperations();
-                if (dm.Search != null && dm.Search.Count > 0)
-                {
-                    DataSource = operation.PerformSearching(DataSource, dm.Search);  //Search
-                }
-                if (dm.Sorted != null && dm.Sorted.Count > 0) //Sorting
-                {
-                    DataSource = operation.PerformSorting(DataSource, dm.Sorted);
-                }
-                if (dm.Where != null && dm.Where.Count > 0) //Filtering
-                {
-                    DataSource = operation.PerformFiltering(DataSource, dm.Where, dm.Where[0].Operator);
-                }
-                if (dm.Skip != 0)
-                {
-                    DataSource = operation.PerformSkip(DataSource, dm.Skip);   //Paging
-                }
-                if (dm.Take != 0)
-                {
-                    DataSource = operation.PerformTake(DataSource, dm.Take);
-                }
-                return dm.RequiresCounts ? Json(new { result = DataSource, count = count }) : Json(DataSource);
-        }
-
-
         public IActionResult StorePersistData([FromBody] StoreData persistData)
         {
-            persistedData = persistData.persistData;
+            persistedData = persistData.persistData; // you can store this to the database. 
             return new JsonResult(persistedData);
         }
 
         public IActionResult Restore()
         {
-            return Content(persistedData);
+            return Content(persistedData); // retrieve the stored layout from the database. 
         }
 
         public class StoreData
@@ -81,19 +52,14 @@ namespace netCore6x.Controllers
         {
 
         }
-        public OrdersDetails(int OrderID, string CustomerId, int EmployeeId, ComplexOrdersDetails Freight, bool Verified, DateTime OrderDate, string ShipCity, string ShipName, string ShipCountry, DateTime ShippedDate, string ShipAddress)
+        public OrdersDetails(int OrderID, string CustomerId, int EmployeeId, double Freight, string ShipCity, string ShipCountry)
         {
             this.OrderID = OrderID;
             this.CustomerID = CustomerId;
             this.EmployeeID = EmployeeId;
             this.Freight = Freight;
             this.ShipCity = ShipCity;
-            this.Verified = Verified;
-            this.OrderDate = OrderDate;
-            this.ShipName = ShipName;
             this.ShipCountry = ShipCountry;
-            this.ShippedDate = ShippedDate;
-            this.ShipAddress = ShipAddress;
         }
         public static List<OrdersDetails> GetAllRecords()
         {
@@ -102,11 +68,11 @@ namespace netCore6x.Controllers
                 int code = 10000;
                 for (int i = 1; i < 5; i++)
                 {
-                    order.Add(new OrdersDetails(code + 1, "ALFKI", i + 0, new ComplexOrdersDetails(0), false, new DateTime(1991, 05, 15), "Berlin", "Simons bistro", "Denmark", new DateTime(1996, 7, 16), "Kirchgasse 6"));
-                    order.Add(new OrdersDetails(code + 2, "ANATR", i + 2, new ComplexOrdersDetails(3.3 * i), true, new DateTime(1990, 04, 04), "Madrid", "Queen Cozinha", "Brazil", new DateTime(1996, 9, 11), "Avda. Azteca 123"));
-                    order.Add(new OrdersDetails(code + 3, "ANTON", i + 1, new ComplexOrdersDetails(0), true, new DateTime(1957, 11, 30), "Cholchester", "Frankenversand", "Germany", new DateTime(1996, 10, 7), "Carrera 52 con Ave. BolĂ­var #65-98 Llano Largo"));
-                    order.Add(new OrdersDetails(code + 4, "BLONP", i + 3, new ComplexOrdersDetails(5.3 * i), false, new DateTime(1930, 10, 22), "Marseille", "Ernst Handel", "Austria", new DateTime(1996, 12, 30), "Magazinweg 7"));
-                    order.Add(new OrdersDetails(code + 5, "BOLID", i + 4, new ComplexOrdersDetails(6.3 * i), true, new DateTime(1953, 02, 18), "Tsawassen", "Hanari Carnes", "Switzerland", new DateTime(1997, 12, 3), "1029 - 12th Ave. S."));
+                    order.Add(new OrdersDetails(code + 1, "ALFKI", i + 0, 0,  "Berlin", "Denmark"));
+                    order.Add(new OrdersDetails(code + 2, "ANATR", i + 2, 3.3 * i,  "Madrid", "Brazil"));
+                    order.Add(new OrdersDetails(code + 3, "ANTON", i + 1, 0, "Cholchester", "Germany"));
+                    order.Add(new OrdersDetails(code + 4, "BLONP", i + 3, 5.3 * i,  "Marseille", "Austria"));
+                    order.Add(new OrdersDetails(code + 5, "BOLID", i + 4, 6.3 * i, "Tsawassen", "Switzerland"));
                     code += 5;
                 }
             }
@@ -116,31 +82,9 @@ namespace netCore6x.Controllers
         public int? OrderID { get; set; }
         public string CustomerID { get; set; }
         public int? EmployeeID { get; set; }
-        public ComplexOrdersDetails? Freight { get; set; }
-        public string ShipCity { get; set; }
-        public bool Verified { get; set; }
-        public DateTime OrderDate { get; set; }
-
-        public string ShipName { get; set; }
-
-        public string ShipCountry { get; set; }
-
-        public DateTime ShippedDate { get; set; }
-        public string ShipAddress { get; set; }
-    }
-
-    public class ComplexOrdersDetails
-    {  
-        public ComplexOrdersDetails()
-        {
-
-        }
-        public ComplexOrdersDetails(double Freight)
-        {
-            this.Freight = Freight;
-        }
-        
-
         public double? Freight { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
     }
+
 }
